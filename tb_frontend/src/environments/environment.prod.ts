9de952788403f6bec681a8e4a088931d976cc140
degaga <degagawolde@gmail.com>
2026-07-15 00:18:55 +0300

    m

--- tb_frontend/src/environments/environment.prod.ts
+++ tb_frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     baseUrl: 'https://products.aii.et/wss/api/',
-    vbaseUrl: 'https://products.aii.et/wss'
+    vbaseUrl: 'https://products.aii.et'
 };
