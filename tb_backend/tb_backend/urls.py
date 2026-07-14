deb1bfb54d8b2e32cd0a6eb2ee33d8ee4e074eab
degaga <degagawolde@gmail.com>
2026-07-15 00:55:46 +0300

    m

--- tb_backend/tb_backend/urls.py
+++ tb_backend/tb_backend/urls.py
@@ -23,7 +23,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('wss/admin/', admin.site.urls),
     path('wss/', include('wss.urls')),
     #path('auth/', include('dj_rest_auth.urls')), 
     #path('auth/registration/', include('dj_rest_auth.registration.urls')),
